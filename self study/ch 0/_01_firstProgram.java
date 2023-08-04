c14481ab00a71c72ea81b8e582c5df52910d4662
Dipto Das <91727309+Diptodas123@users.noreply.github.com>
2023-08-04 17:56:25 +0530

    Add files via upload

---
+++ self study/ch 0/_01_firstProgram.java
@@ -0,0 +1,10 @@
+class _01_firstProgram{
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
+
+
+
+//addTwoNumbers --> camelCaseConvention
+//AddTwoNumbers --> PascalConvention
